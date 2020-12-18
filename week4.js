e7a6d411518138a953f1ddd670036fa4392b583a
AmarRed07 <redzepagicamar@yahoo.com>
2020-12-17 21:59:48 -0800

    just an update

--- week4.js
+++ week4.js
@@ -14,35 +14,32 @@ console.log (differentName('Different First Name ', 'Different Last Name'));
 
 setTimeout(() => alert('Time is up! '), 2);
 
-
-// b. // b.	Write an arrow function named askAreWeThereYet that alerts ‘Are we there yet?’. 
+// b.	Write an arrow function named askAreWeThereYet that alerts ‘Are we there yet?’. 
 //  call setInterval,pass askAreWeThereYet into the callback parameter position. Choose whatever length of time you want for 
 //  the interval.
 
-
 askAreWeThereYet = setInterval (() => alert('Are we there yet? '), 5000);
-
-
-
 
 
 // 4. In this step you are going to write a function that takes a callback to better understand how callbacks work.
 
 // a, b, c
 
-
-// This is not an arrow function, this is a basic function, being 'called' <-- outside the brackets.
-
 const sportsArray = ['football', 'basketball', 'baseball'];
+const elementSpliced = sportsArray.splice(1, 1);
 
-const deleted = sportsArray.splice(1, 1);
-
-function processSplicedValue(firstChair, secondPar, sportsArray) {
-    console.log('string', 'this does not matter', firstChair, secondPar, sportsArray); 
+function processSplicedValue(sportsArray, index, callBack) {
+    console.log(sportsArray); 
 }
-processSplicedValue('baseball', 'football');
-console.log(deleted);
+processSplicedValue(() => { console.log(sportsArray, elementSpliced)});
 
+
+// -----------------------------------------------------------------------------------
+// 5:
+
+// a.
 alert(processSplicedValue, 5000);
-
+// b.
 setTimeout(() => alert(processSplicedValue), 5000);
+// c.
+console.log(processSplicedValue(elementSpliced)); // confused
